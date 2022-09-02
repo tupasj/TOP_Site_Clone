b3e06f3d7fbcbeb8aaff797b6e942b98ea6f157e
Jeremy Tupas <jeremytupas@gmail.com>
2022-09-01 19:38:52 -0500

    Revise SASS structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import './App.scss';
+import './styles.scss';
+// import axios from "axios";
 
 const App = () => {
+  // const twitchAPI = axios.create({
+  //   headers: {
+  //     Authorization: "Bearer veemgsu384i20g5yp8yjrb04198zlz",
+  //     "Client-Id": "95n6aprvq0p87raezjptqic51b6944",
+  //   },
+  // });
+
   return (
     <>
     <p>lorem ipsum</p>
