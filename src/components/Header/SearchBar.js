091bff3488b90fd885de95386a519ee6886d5d1e
Jeremy Tupas <jeremytupas@gmail.com>
2022-09-13 18:04:04 -0500

    Added search bar css

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,8 +1,12 @@
 const SearchBar = () => {
     return (
         <div className="searchbar">
-            <input className="searchbar__input" type="text" placeholder="Search"></input>
-            <i className="searchbar__icon--magnifying-glass fa-solid fa-magnifying-glass"></i>
+            <div className="searchbar__container">
+                <input className="searchbar__input" type="text" placeholder="Search"></input>
+                <div className="searchbar__icon-container">
+                    <i className=" searchbar__icon--magnifying-glass fa-solid fa-magnifying-glass"></i>
+                </div>               
+            </div>
         </div>
     );
 };
