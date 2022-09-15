a4f93813079e193f43569a5acc06ef3eaae3b935
Jeremy Tupas <jeremytupas@gmail.com>
2022-09-14 19:52:13 -0500

    Added livestreamer search functionality

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,14 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const SearchBar = () => {
-    return (
-        <div className="searchbar">
-            <div className="searchbar__container">
-                <input className="searchbar__input" type="text" placeholder="Search"></input>
-                <div className="searchbar__icon-container">
-                    <i className="searchbar__icon--magnifying-glass fa-solid fa-magnifying-glass"></i>
-                </div>               
-            </div>
+  const [searchText, setSearchText] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    if (e.key === "Enter" || e.type !== "keydown") {
+      navigate(`/channel/${searchText}`);
+    };
+  };
+
+  return (
+    <div className="searchbar">
+      <div className="searchbar__container">
+        <input
+          className="searchbar__input"
+          type="text"
+          placeholder="Search streamer"
+          value={searchText}
+          onChange={handleChange}
+          onKeyDown={handleSubmit}
+        ></input>
+        <div className="searchbar__icon-container" onClick={handleSubmit}>
+          <i className="searchbar__icon--magnifying-glass fa-solid fa-magnifying-glass"></i>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export { SearchBar };
